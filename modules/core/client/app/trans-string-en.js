11fa6d762bbd6a1285d7b5cecde3d4acaed5ba54
pmwoodward3 <pmwoodward3@users.noreply.github.com>
2017-05-18 09:35:48 -0400

    Updated English strings
    I found and fixed some issues in the english translation.

--- modules/core/client/app/trans-string-en.js
+++ modules/core/client/app/trans-string-en.js
@@ -36,9 +36,9 @@
     },
 
     //client menu
-    SIGNOUT: 'Signout',
-    SIGNIN: 'Sign In',
-    SIGNUP: 'Sign Up',
+    SIGNOUT: 'Logout',
+    SIGNIN: 'Login',
+    SIGNUP: 'Register',
     EDIT_PROFILE: 'Edit Profile',
     EDIT_PROFILE_PIC: 'Edit Profile Picture',
     CHANGE_PASSWORD: 'Change Password',
@@ -51,26 +51,26 @@
     UPLOAD_RULES_CONTENT: [
       'The torrent file`s Tracker URL must be: <mark><strong>{{url}}</strong></mark>.',
       'The torrent file`s TMDB_ID must be TheMovieDB resources ID, <strong><a href="https://www.themoviedb.org/" target="_blank">you can find the ID from here</a></strong>.',
-      'The resources detail info can be autoload, if it`s fine, then you can submit it.',
+      'The resources detail info is pulled from the ID, if everything looks good, hit submit. ',
       'Select one or more tags that match the resources, which will play a significant role in your search results.',
-      'After the submission of documents, may be approved by the management, the rules do not meet the seeds will be deleted directly.',
+      'After submission, the upload may need to be approved by management, if it does not meet our requirments or is objectionable it will be deleted directly.',
       'For additional assistance, please contact our administrator: <strong><a href="mailto:#">{{admin}}</a></strong>.'
     ],
 
     //HomeController & home views
-    LOADING_TOP: 'Loading recommended info, please waiting...',
-    TOP_MOVIE_INFO_ERROR: 'Get movie top info failed',
+    LOADING_TOP: 'Loading recommended info, please wait...',
+    TOP_MOVIE_INFO_ERROR: 'Top Movie info retrieval failed',
     NEWEST_MOVIE_LIST: 'Newest Movie Torrents',
 
     //element title/alt
     TITLE_ALT: {
-      SEEDS: 'Seeds users',
-      LEECHERS: 'Leechers users',
-      FINISHED: 'Finished users',
+      SEEDS: 'Seeds',
+      LEECHERS: 'Leechers',
+      FINISHED: 'Finished',
       IMDB_VOTES: 'IMDB Votes',
       MORE_TAGS: 'Show More Search Tags',
       RESET_TAGS: 'Reset All Search Tags',
-      DOWNLOAD_TORRENT: 'Doanload the torrent'
+      DOWNLOAD_TORRENT: 'Download torrent'
     },
 
     //table fields
@@ -79,15 +79,15 @@
       INFO: 'Torrent info',
       SIZE: 'Size',
       SEEDS_LEECHERS_FINISHED: 'S/L/F',
-      PUBLISHER: 'Publisher',
+      PUBLISHER: 'Uploader',
       ADMIN_TOOLS: 'Admin Tools',
       LIFETIME: 'Life',
       VOTES: 'Votes',
 
       //peer
       USERNAME: 'DisplayName',
-      UPLOADED: 'Uploaded/Speed',
-      DOWNLOADED: 'Downloaded/Speed',
+      UPLOADED: 'Uploaded',
+      DOWNLOADED: 'Downloaded',
       RATIO: 'Ratio',
       FINISHED: 'Finished',
       STARTED: 'Started',
@@ -116,14 +116,14 @@
       ADMIN_USER_LIST: 'User List',
       ADMIN_USER_VIEW: 'View User',
       ADMIN_USER_EDIT: 'Edit User',
-      ADMIN_TORRENTS_LIST: 'Torrents LIst',
+      ADMIN_TORRENTS_LIST: 'Torrents List',
       PASSWORD_FORGOT: 'Password forgot',
       PASSWORD_RESET: 'Password reset'
     },
 
     //TorrentsController & views
-    MOVIE_PAGE_INFO_ERROR: 'Get movie page info failed',
-    MOVIE_PAGE_INFO_EMPTY: 'No movie founded, please change some new tags to research',
+    MOVIE_PAGE_INFO_ERROR: 'Movie info retrieval failed',
+    MOVIE_PAGE_INFO_EMPTY: 'Movie info not found',
     TAGS_SEARCH: 'Tags Search',
     CA_KEYWORD: 'Keyword',
     CA_TORRENT_STATUS: 'Torrent status',
@@ -133,7 +133,7 @@
     CA_RESET: 'Reset',
     CA_DOWNLOAD: 'Download',
     TORRENT_DOWNLOAD_ERROR: 'Torrent file download failed',
-    TORRENTS_DOWNLOAD_SUCCESSFULLY: 'Torrents file download successfully',
+    TORRENTS_DOWNLOAD_SUCCESSFULLY: 'Torrent file download successful',
 
     //torrent info
     UNIT_MILLION: 'million',
@@ -157,10 +157,10 @@
     SUBTITLE_LIST: 'Subtitle list',
     SUBTITLE_RULES: {
       0: 'If the torrent has no subtitle files, you have the following actions.',
-      1: 'Please note the filename format, such as: <mark>Sing.2016.3D.HKG.BluRay.1080p.AVC.Atmos.TrueHD7.1-MTeam.chs&eng.srt</mark>'
+      1: 'Please note the filename format, such as: <mark>Movie (20XX) RESp.eng/chi.srt/ass</mark>'
     },
     MY_TORRENT_RULES: {
-      0: 'This torrent file is uploaded by yourself, you have actions bellow.',
+      0: 'This torrent file is uploaded by you, you have actions bellow.',
       1: 'For additional assistance, please contact our administrator: <strong><a href="mailto:#">{{admin}}</a></strong>'
     },
     ENTER_VIDEO_NFO: 'Please enter video NFO',
@@ -171,13 +171,13 @@
     SUBTITLE_DELETE_ICON_TITLE: 'Delete this subtitle',
     SUBTITLE_CONFIRM_OK: 'Delete',
     SUBTITLE_CONFIRM_CANCEL: 'Cancel',
-    SUBTITLE_CONFIRM_HEADER_TEXT: 'Delete Confirm',
+    SUBTITLE_CONFIRM_HEADER_TEXT: 'Confirm Deletion',
     SUBTITLE_CONFIRM_BODY_TEXT: 'Are you sure want to delete this subtitle?',
     SUBTITLE_DOWNLOAD_SUCCESSFULLY: 'Subtitle file download successfully',
     SUBTITLE_DOWNLOAD_ERROR: 'Subtitle file download failed',
     SUBTITLE_DELETE_SUCCESSFULLY: 'Subtitle file delete successfully',
-    SUBTITLE_DELETE_ERROR: 'Subtitle file delete failed',
-    OTHER_TORRENT_LIST_TITLE: 'This resources has {{x}} other version torrent, you can view or download from here:',
+    SUBTITLE_DELETE_ERROR: 'Subtitle file deletion failed',
+    OTHER_TORRENT_LIST_TITLE: 'This resources has {{x}} other versions, you can view or download from here:',
 
     ADMIN_BASIC_COMMAND: 'Basic Command',
     ADMIN_BASIC_REVIEWED: 'Reviewed',
@@ -190,10 +190,10 @@
 
     TORRENT_DELETE_CONFIRM_OK: 'Delete',
     TORRENT_DELETE_CONFIRM_CANCEL: 'Cancel',
-    TORRENT_DELETE_CONFIRM_HEADER_TEXT: 'Delete Confirm',
+    TORRENT_DELETE_CONFIRM_HEADER_TEXT: 'Confirm Deletion',
     TORRENT_DELETE_CONFIRM_BODY_TEXT: 'Are you sure want to delete this torrent?',
     TORRENT_DELETE_SUCCESSFULLY: 'Torrent delete successfully',
-    TORRENT_DELETE_ERROR: 'Torrent delete failed',
+    TORRENT_DELETE_ERROR: 'Torrent deletion failed',
     TORRENT_UPDATE_CONFIRM_OK: 'Update',
     TORRENT_UPDATE_CONFIRM_CANCEL: 'Cancel',
     TORRENT_UPDATE_CONFIRM_HEADER_TEXT: 'Update Confirm',
@@ -227,7 +227,7 @@
     COMMENT_EDITED_INFO: 'Edit at',
     COMMENT_CONFIRM_OK: 'Delete',
     COMMENT_CONFIRM_CANCEL: 'Cancel',
-    COMMENT_CONFIRM_HEADER_TEXT: 'Delete Confirm',
+    COMMENT_CONFIRM_HEADER_TEXT: 'Confirm Deletion',
     COMMENT_CONFIRM_BODY_TEXT: 'Are you sure want to delete this comment?',
     COMMENT_CONFIRM_BODY_TEXT_REPLY: 'Are you sure want to delete this comment reply?',
     COMMENT_EDIT_ICON_TITLE: 'Edit this reply',
@@ -244,13 +244,13 @@
     ENTER_TMDB_ID: 'Please enter theMovieDB id 　<span style="font-size: 10pt;">[<a href="https://www.themoviedb.org/" target="_blank">find id on themofiedb.org</a>]</span>',
     LOAD_TMDB_INFO: 'Load info',
     TMDB_ID: 'TMDB ID',
-    TMDB_ID_OK: 'TMDB ID is ok! Get info successfully',
+    TMDB_ID_OK: 'TMDB ID retrival successful',
     TMDB_ID_ERROR: 'TMDB ID is error! Get info failed',
     TMDB_ID_REQUIRED: 'Please enter TMDB ID',
     TMDB_MOVIE_INFO: 'The movie info from TMDB',
-    TMDB_IS_LOADING: 'Loading the movie info, please wait...',
-    SELECT_TAGS: 'Please select any tags for the resources',
-    TORRENTS_SUBMIT_UPLOAD: 'Agree the rules and submit your resources',
+    TMDB_IS_LOADING: 'Loading movie info, please wait...',
+    SELECT_TAGS: 'Please select any tags for the resource',
+    TORRENTS_SUBMIT_UPLOAD: 'Agree the rules and submit your resource',
     SUBMIT_BUTTON: 'OK, SUBMIT NOW',
     CANCEL_BUTTON: 'NO, CANCEL IT',
     AGREE_RULES: 'I agree and already read all the rules, <a href="#">read here</a>',
@@ -296,7 +296,7 @@
     CAPTION_REQUIRED_VERIFY_PASSWORD: 'Verify your new password.',
     CAPTION_PASSWORD_NOT_MATCH: 'Passwords do not match.',
     CURRENT_PASSKEY: 'Current Passkey: <mark class="text-danger">{{passkey}}</mark>',
-    RESET_PASSKEY_NOTE: 'NOTE: If you reset your passkey, you must re-download all torrent file, and re-add to your download client, it can download or upload.',
+    RESET_PASSKEY_NOTE: 'NOTE: Your passkey is unique, if you reset your passkey, you must re-download all torrent files, and re-add them to your download client.',
 
     RESET_PASSKEY_CONFIRM_OK: 'Reset',
     RESET_PASSKEY_CONFIRM_CANCEL: 'Cancel',
@@ -307,7 +307,7 @@
 
     //chat view
     CHAT_USERS_LIST: 'Users List',
-    CHAT_WELCOME: 'Welcome to join the chat room, please follow the chat rules, and have a good time!',
+    CHAT_WELCOME: 'Welcome to the chat room, please follow the rules, and have a good time!',
     CHAT_PLACEHOLDER_INPUT: 'input new message and press enter',
     CHAT_USER_JOIN: 'is now connected and join the room.',
     CHAT_USER_QUIT: 'is now disconnect and quit the room.',
@@ -318,8 +318,8 @@
     CHAT_FONT_BOLD: 'Font Bold',
     CHAT_FONT_ITALIC: 'Font Italic',
     CHAT_BAN_KICK: 'Kick out and ban IP',
-    CHAT_BAN_KICK_REASON: 'you are not grateful at here',
-    CHAT_BAN_KICK_MESSAGE: '{{who}} was kicked and ban by {{by}} ({{reason}})',
+    CHAT_BAN_KICK_REASON: 'Contant site management.',
+    CHAT_BAN_KICK_MESSAGE: '{{who}} was kicked or ban by {{by}} ({{reason}})',
 
     CHAT_CONFIRM_BAN_OK: 'Ban & Kick',
     CHAT_CONFIRM_BAN_CANCEL: 'Cancel',
